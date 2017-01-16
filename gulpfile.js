6a48dfb503a4d50cae129f5fd195107b03f9f4e2
Giovani Soave <tec.giovani@hotmail.com>
2017-01-16 16:08:06 -0200

    End of activity

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+//code for minify and compile code by Giovani Soave - 2017
+
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-html-minifier');
+
+
+gulp.task('sass', function () {
+    return gulp.src('./source/scss/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('minify', function() {
+    gulp.src('./source/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./source/scss/**/*.scss', ['sass']);
+    gulp.watch('./source/*.html', ['minify']);
+});
