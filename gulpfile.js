7baf9b45c1d1b36987f5e1f686782806f648e9c8
Keven Jesus <falecom@keven.com.br>
2017-01-18 11:09:37 -0200

    adicionado plugin htmlmin

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
-var htmlmin = require('gulp-html-minifier');
+var htmlmin = require('gulp-htmlmin');
 
 
 gulp.task('sass', function () {
